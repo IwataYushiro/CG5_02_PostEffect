a3e214cb9281542c86ab8fa082bb83ddc66cefdc
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-05-24 12:44:41 +0900

    縮小バッファ＋ガウシアンブラー

--- Assets/05/SimpleBlur.cs
+++ Assets/05/SimpleBlur.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class SimpleBlur : MonoBehaviour
 {
-    //public Shader shader;
-    //private Material material;
-    //private void Awake()
-    //{
-    //    material = new Material(shader);    //シェーダーを割り当てる
-    //}
+    public Shader shader;
+    private Material material;
+    private void Awake()
+    {
+        material = new Material(shader);    //シェーダーを割り当てる
+    }
     
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
@@ -17,17 +17,22 @@ public class SimpleBlur : MonoBehaviour
         RenderTexture buf1 = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0, source.format);
         RenderTexture buf2 = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0, source.format);
         RenderTexture buf3 = RenderTexture.GetTemporary(source.width / 8, source.height / 8, 0, source.format);
+        //シェーダー適応用バッファ
+        RenderTexture blurTex = RenderTexture.GetTemporary(buf3.width, buf3.height, 0, buf3.format);
+        
         //ダウンサンプリング
         Graphics.Blit(source, buf1);
         Graphics.Blit(buf1, buf2);
         Graphics.Blit(buf2, buf3);
+        Graphics.Blit(buf3, blurTex, material);
         //アップサンプリング
-        Graphics.Blit(buf3, buf2);
+        Graphics.Blit(blurTex, buf2);
         Graphics.Blit(buf2, buf1);
         Graphics.Blit(buf1, destination);
         //確保したらリリースしようね
         RenderTexture.ReleaseTemporary(buf1);
         RenderTexture.ReleaseTemporary(buf2);
         RenderTexture.ReleaseTemporary(buf3);
+        RenderTexture.ReleaseTemporary(blurTex);
     }
 }
